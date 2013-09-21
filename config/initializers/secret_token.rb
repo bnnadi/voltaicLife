bf7b214732470448a0d2b3cc465a70197142e3cf
Bisike <bisike.nnadi@gmail.com>
2013-09-20 23:45:43 -0400

    First commit with status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VoltaicLife::Application.config.secret_key_base = '835b8308bae84d9eaa29caaae5aa1071da2f7385b79d87b366b096134b10762c560b67aa5714c89672b29bd686e05fb82085d9424a381292e178beb02b8fbe80'
