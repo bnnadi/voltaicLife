7e71065e63c8b83d363c01259b57b9ea21852c08
Bisike <bisike.nnadi@gmail.com>
2013-09-23 21:57:14 -0400

    added pictures for welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
+require 'net/http'
+
 class WelcomeController < ApplicationController
   def index
-
+    API_Key = 'xbg5na4bnkpmyqs5f7zdbrnn'
+    uriEvents = URI('http://api.jambase.com/events?')
+    uriArtist = URI('http://api.jambase.com/artists?')
+    params = {:api_key => API_Key, :}
+       Net::HTTP.get()
   end
 end
