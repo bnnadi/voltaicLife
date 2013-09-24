f30ed1d0fa82cabca31452195a531e639c625d1e
Bisike <bisike.nnadi@gmail.com>
2013-09-23 20:13:37 -0400

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name, :full_name, :avatar
+  
+  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+ 
+  has_many :statuses
+ 
+  def full_name
+    first_name  + " " + last_name
+  end
 end
