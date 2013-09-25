581451b2a8371c61dc470be60f34469725aa80a6
Bisike <bisike.nnadi@gmail.com>
2013-09-24 22:43:18 -0400

    started working on the tests to see why the only email and password is being saved to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :avatar, :first_name, :last_name, :profile_name,  :email, :password, :password_confirmation, :remember_me, :full_name
+ 
+ 
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar, :first_name, :last_name, :profile_name,  :full_name
   
   has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
+  validates :first_name, presence: true
+  
+  validates :last_name, presence: true
+  
+  validates :profile_name, presence: true,  uniqueness: true
  
   has_many :statuses
  
--- db/migrate/20130921054716_devise_create_users.rb
+++ db/migrate/20130921054716_devise_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
-      t.string :first_name
+      t.string :first_name, :null => false, :default => ""
       t.string :last_name
       t.string :profile_name
       
